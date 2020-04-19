9f07a318770dbe5e5a363873b8580c56e6205e95
amitabh <buddhabacchan@gmail.com>
2020-04-19 10:30:51 -0700

    created module structure

--- psmodules/AmitabhCo-Hyper-V/Public/Init-Hyper-V.ps1
+++ psmodules/AmitabhCo-Hyper-V/Public/Init-Hyper-V.ps1
@@ -1,8 +1,13 @@
-function Create-External-Switch {
+# Define external switch name
+$ExternalSwitchName = "ExternalVirtualSwitch"
+
+function New-External-Switch {
+
+    # If the switch does not exists then create it
     $ExternalSwitches = Get-VMSwitch -SwitchType External
     if ($null -eq $ExternalSwitches) {
     
-        # Identify the phusical Network adapter
+        # Identify the physical Network adapter
         $NetAdapter = Get-NetAdapter -Physical
         if ($null -eq $NetAdapter) {
             Write-Error "No Physical Network Adapter Found...Aborting"
@@ -10,14 +15,21 @@ function Create-External-Switch {
         }
 
         # Check if external switch is already created
-        $ExternalSwitchName = "ExternalSwitch"
+        
 
         # Create a new External Switch
         New-VMSwitch -name $ExternalSwitchName  -NetAdapterName $NetAdapter -AllowManagementOS $true
     }
+
+    # If the switch is already created and if it is not with the same name
+    # then delete it and re-create it or try to rename it
     else {
     
     }
 }
+
+Export-ModuleMember -Variable $ExternalSwitchName
+Export-ModuleMember -Function New-External-Switch
+
 
 
